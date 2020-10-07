2474d24819508a0da457ba31c52b075b9914f822
Carles Perez Lopez <carlesperez94@gmail.com>
2020-10-07 18:22:39 +0200

    Reaction patterns can be found through substructural searching

--- covalent_linker/Reactions/reactions.py
+++ covalent_linker/Reactions/reactions.py
@@ -13,19 +13,23 @@ class Reactant:
         self.molecule_type = None
         self.content = None
         self.rdkit_mol = None
+        self.pattern = None
+        self.reactant_atoms = None
     
     def load_ligand(self, pdb_input, ligand_chain="L"):
         self.pdb = PDB(pdb_input)
         self.pdb_file = pdb_input
-        self.content = self.pdb.get_ligand(ligand_chain)
+        self.pdb.read_conect() # We need conectivity to get BONDS
+        self.content = self.pdb.get_ligand(ligand_chain) + "\n" + "".join(self.pdb.conect_section) # List -> Str
         print("Ligand:")
         print(self.content)
         self.molecule_type = "ligand"
 
     def load_residue(self, pdb_input, residue_number, residue_chain):
         self.pdb = PDB(pdb_input)
+        self.pdb.read_conect()
         self.pdb_file = pdb_input
-        self.content = self.pdb.get_residue(residue_number, residue_chain)
+        self.content = self.pdb.get_residue(residue_number, residue_chain) + "\n" + "".join(self.pdb.conect_section)
         print("Residue:")
         print(self.content)
         self.molecule_type = "residue"
@@ -38,15 +42,33 @@ class Reactant:
         self.rdkit_mol = Chem.MolFromPDBFile(tmp_filename, removeHs=False)
         os.remove(tmp_filename)
 
+    def search_pattern(self):
+        self.transform_to_rdkit()
+        patt = Chem.MolFromSmarts(self.pattern) 
+        self.reactant_atoms = self.rdkit_mol.GetSubstructMatch(patt)
+
 
 class Reaction:
     """
     Pattern class to define reactions.
     """
     def __init__(self):
-        self.reactant1 = None
-        self.reactant2 = None
+        self.reactants = []
         self.reaction_type = None
+
+    def set_triple_bond_addition_reaction(self):
+        if len(self.reactants) != 2:
+            raise ValueError("You need two reactants in this reaction")
+        self.reaction_type = "Triple bond addition"
+        self.reactants[0].pattern = "[NX1]#[CX2]"
+        self.reactants[1].pattern = "[#16X2H]"
+
+    def get_atoms_from_reaction(self):
+        for react in self.reactants:
+            react.search_pattern()
+            print("Reaction atoms for {} {}".format(react.molecule_type, 
+                                                    react.reactant_atoms))
+
 
 class LigandResidueReaction(Reaction):
     """
@@ -56,13 +78,18 @@ class LigandResidueReaction(Reaction):
         super().__init__()
 
     def load_pdb(self, pdb_file, residue_chain, residue_num, ligand_chain="L"):
-        self.reactant1 = Reactant()
-        self.reactant1.load_ligand(pdb_file, ligand_chain)
-        self.reactant2 = Reactant()
-        self.reactant2.load_residue(pdb_file, residue_num, residue_chain) 
+        r1 = Reactant()
+        r1.load_ligand(pdb_file, ligand_chain)
+        self.reactants.append(r1)
+        r2 = Reactant()
+        r2.load_residue(pdb_file, residue_num, residue_chain) 
+        self.reactants.append(r2)
+        
 
 def create_temporary_file(content, filename):
     with open(filename, "w") as out:
         out.write(content)
+
+
 
 
