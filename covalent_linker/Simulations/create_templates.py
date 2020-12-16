5ffa30216bab082cf1f5abbf8c08b5098d9f9508
Carles Perez Lopez <carlesperez94@gmail.com>
2020-12-16 16:50:15 +0100

    peleffy functions updated to the last version. Degrees of rotation can be also included as variable

--- covalent_linker/Simulations/create_templates.py
+++ covalent_linker/Simulations/create_templates.py
@@ -1,5 +1,6 @@
 import os
-from peleffy.topology import Molecule, RotamerLibrary
+from peleffy.topology import Molecule, Topology, RotamerLibrary
+from peleffy.forcefield import OpenForceField, OPLS2005ForceField
 from peleffy.template import Impact
 from peleffy.utils import get_data_file_path
 import frag_pele.Covalent.correct_template_of_backbone_res as cov
@@ -32,13 +33,19 @@ class ResidueLigandTemplate:
                                 self.aminoacid.lower()) # Aminoacids are not parametrized in OFF yet.
         return path
 
-    def __get_template_and_rot(self, template_path='grw', rot_path='GRW.rot.assign'):
+    def __get_template_and_rot(self, template_path='grw', rot_path='GRW.rot.assign', rot_res=30):
         os.environ['SCHRODINGER'] = self.sch_path
         m = Molecule(self.ligand_pdb, 
-                     core_constraints=[' CA ', ' C  ', ' N  ']) 
-        m.parameterize(self.__forcefield)
-        impact = Impact(m)
-        impact.write(template_path)
+                     core_constraints=[' CA ', ' C  ', ' N  '],
+                     rotamer_resolution=rot_res)
+        if self.__forcefield == 'OPLS2005':
+            ff = OPLS2005ForceField()
+        if self.__forcefield == 'OpenForceField': # Not tested yet
+            ff = OpenForceField('openff_unconstrained-1.2.0.offxml') 
+        parameters = ff.parameterize(m)
+        topology = Topology(m, parameters)
+        impact = Impact(topology)
+        impact.to_file(template_path)
         aa_template = self.__create_aa_template_path()  
         cov.correct_template(template_path, aa_template)
         print("Template modified in {}.".format(template_path))
@@ -46,7 +53,7 @@ class ResidueLigandTemplate:
         rotamer_library.to_file(rot_path)
         print("Rotamer library stored in {}".format(rot_path))
 
-    def get_datalocal(self, outdir=".", name="grw"):
+    def get_datalocal(self, outdir=".", name="grw", rot_res=30):
         folder_handler.check_and_create_DataLocal(working_dir=outdir)
         if self.__forcefield == 'OPLS2005':
             datalocal_temp_path = os.path.join(outdir,  
@@ -59,6 +66,7 @@ class ResidueLigandTemplate:
         datalocal_rot_path = os.path.join(outdir, "DataLocal/LigandRotamerLibs", 
                                           "{}.rot.assign".format(name.upper()))
         self.__get_template_and_rot(template_path=datalocal_temp_path, 
-                                    rot_path=datalocal_rot_path)
+                                    rot_path=datalocal_rot_path,
+                                    rot_res=rot_res)
 
  
